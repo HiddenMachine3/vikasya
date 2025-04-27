26710f025d26420031db337a21e8477a17f039c7
AKxy4321 <adityakushal23@gmail.com>
2025-04-27 14:59:13 +0530

    hello6

--- tf_lite_converter.py
+++ tf_lite_converter.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 
+model_name = "final_model"  # or .keras or SavedModel
+tf_lite_model_name = "best_model"  # or .keras or SavedModel
+
 # Load your Keras model
-model = tf.keras.models.load_model(
-    "best_model_12.46_89_test.keras"
-)  # or .keras or SavedModel
+model = tf.keras.models.load_model(f"{model_name}.keras")  # or .keras or SavedModel
 
 # Set up the TFLite Converter
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
@@ -15,5 +16,5 @@ converter.optimizations = []
 tflite_model = converter.convert()
 
 # Save the TFLite model
-with open("best_model_12.46_89_test.tflite", "wb") as f:
+with open(f"{tf_lite_model_name}.tflite", "wb") as f:
     f.write(tflite_model)
